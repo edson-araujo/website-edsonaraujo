261f555ff64149e560063e5a95f9fe842e345d2a
edson-araujo <edson-456@hotmail.com>
2025-01-13 20:01:30 -0300

    ajustes

--- components/links/grid-items/equipments.tsx
+++ components/links/grid-items/equipments.tsx
@@ -3,15 +3,10 @@ import Image from "next/image";
 import Link from "next/link";
 import Icon from "../icon";
 import Button from "../button";
-import { useRouter } from 'next/navigation';
 
 const Equipments = ({ item }: { item: GridItemInterface }) => {
-  const router = useRouter();
-
-  return (
-    <div className="w-full h-min overflow-hidden rounded-3xl"  
-    onClick={item.layout === "2x4" ? () => window.open(item.buttonLink ?? "/", "_blank") : undefined}
-    style={{ cursor: item.layout === "2x4" ? "pointer" : "default" }}>
+  return ( <Link href={item.buttonLink ?? ""}>
+    <div className="w-full h-min overflow-hidden rounded-3xl">
       <div className="flex flex-col md:flex-row w-full">
         {/* Content */}
         <div className="relative z-20 flex flex-col justify-between h-full w-full p-4 sm:p-8">
@@ -65,6 +60,7 @@ const Equipments = ({ item }: { item: GridItemInterface }) => {
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
